8a4a918e6ebbcbe7781de5542bae4f991ccd9018
faizan.abbas@nutech.edu.pk <faizan.abbas@nutech.edu.pk>
2022-07-18 13:41:03 +0500

    fix country api

--- lib/models/country.dart
+++ lib/models/country.dart
@@ -8,4 +8,12 @@ class Country {
     required this.code,
     required this.name,
   });
+
+  factory Country.fromJson(Map<String, dynamic> json) {
+    return Country(
+      id: json['id'],
+      code: json['code'],
+      name: json['name'],
+    );
+  }
 }
